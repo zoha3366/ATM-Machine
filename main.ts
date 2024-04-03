9d6be32f05d8b88ac17875db14bfad08d168b0a6
Zoha Qayoom <zohaqayoom5@gmail.com>
2024-04-04 00:29:34 +0500

    publish

---
+++ main.ts
@@ -0,0 +1,60 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+let myBalance = 20000;
+let myPin = 2323;
+
+console.log("WELCOME TO ATM");
+
+let pinAnswer = await inquirer.prompt([
+  {
+    message: "Enter your pin number:",
+    type: "number",
+    name: "Pin",
+  },
+]);
+
+if (pinAnswer.Pin === myPin) {
+  console.log("correct pin code!");
+
+let operation = await inquirer.prompt([
+  {
+    name: "operation",
+    messege: "Please select one option",
+    type: "list",
+    choices: ["Withdraw", "Check available balance"],
+  },
+]);
+
+if (operation.operation === "Withdraw") {
+  let amount = await inquirer.prompt([
+    {
+      name: "amount",
+      message: "Enter your amount",
+      type: "number",
+    },
+  ]);
+  if(amount.amount >= myBalance){
+   console.log ("Insufficient Balance");
+  }else{
+  myBalance -= amount.amount;
+  console.log (`Your Availble Balance is ${myBalance}`)}
+}
+
+
+ else if (operation.operation === "Check available balance"){
+   console.log (`Your Available Balance is ${myBalance}`)
+}
+}
+else {
+   console.log("Incorrect pin code TRY AGAIN");
+}
+
+
+
+
+
+ 
+
+
+
